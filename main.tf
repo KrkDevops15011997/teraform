b4164e59b0dd12cd7813e1adaf879c6f3c02df72
KrkDevops15011997 <krishnadevops1997@gmail.com>
2025-06-26 13:09:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,3 +41,29 @@ resource "google_compute_instance" "simple_vm" {
   name         = "simple-vm"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
+
+boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+ 
+  network_interface {
+    network    = google_compute_network.simple_vpc.name
+    subnetwork = google_compute_subnetwork.simple_subnet.name
+    access_config {} # Allocates an external IP
+  }
+ 
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    echo "Hello from startup script" > /var/log/startup-script.log
+    date >> /var/log/startup-script.log
+  EOT
+ 
+  tags = ["http-server"]
+}
+ 
+# Output the External IP
+output "external_ip" {
+  value = google_compute_instance.simple_vm.network_interface[0].access_config[0].nat_ip
+}
